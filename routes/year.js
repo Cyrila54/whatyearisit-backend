f9137908f86e01cef61d1c8ad17e89b0184867bf
Cyrila54 <cyril.a54@icloud.com>
2023-01-20 13:08:00 +0100

    message

--- routes/year.js
+++ routes/year.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/year', function(req, res, next) {
+router.get('/', function(req, res, next) {
     let year = new Date();
     year=year.getFullYear().toString();
 
